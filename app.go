04e4e949954c2a460f76f2075ef699ecc1df547e
MZ <modstart@163.com>
2026-02-20 10:47:12 +0800

    feat: test publish

--- app.go
+++ app.go
@@ -504,7 +504,7 @@ var appConfig = struct {
 	Name:            "ProcHub",
 	Title:           "ProcHub",
 	Slogan:          "Manage processes easily",
-	Version:         "v0.5.2",
+	Version:         "v0.5.3",
 	Website:         baseURL,
 	WebsiteGithub:   "https://github.com/modstart-lib/prochub",
 	WebsiteGitee:    "https://gitee.com/modstart-lib/prochub",
